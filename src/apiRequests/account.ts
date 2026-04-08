b14e697638549f607e7ddabfb74d1b3d8761f9dd
HoangKyPham <hoangkypham1682@gmail.com>
2026-04-08 17:00:30 +0700

    create-api-account

--- src/apiRequests/account.ts
+++ src/apiRequests/account.ts
@@ -1,11 +1,16 @@
+import AddEmployee from "@/app/manage/accounts/add-employee";
 import http from "@/lib/http";
-import { AccountResType } from "@/schemaValidations/account.schema";
+import { AccountListResType, AccountResType, CreateEmployeeAccountBodyType, UpdateEmployeeAccountBodyType } from "@/schemaValidations/account.schema";
 
 
-
+const prefix = '/accounts'
 const accountApiRequest = {
-    me : () => http.get<AccountResType>("account/me")
-    
+    me : () => http.get<AccountResType>(`${prefix}/me`),
+    list : () => http.get<AccountListResType>(`${prefix}/list`),
+    addEmployee : (body : CreateEmployeeAccountBodyType) => http.post<AccountResType>(prefix, body),
+    updateEmployee: (id: number, body: UpdateEmployeeAccountBodyType) => http.put<AccountResType>(`${prefix}/detail/${id}`, body),
+    deleteEmployee: (id: number) => http.delete<AccountResType>(`${prefix}/detail/${id}`),
+    getEmployee : (id: number) => http.get<AccountResType>(`${prefix}/detail/${id}`)
 }
 
 export default accountApiRequest;
--- src/schemaValidations/account.schema.ts
+++ src/schemaValidations/account.schema.ts
@@ -8,6 +8,14 @@ export const AccountSchema = z.object({
   avatar: z.string().nullable()
 })
 
+export type AccountType = z.TypeOf<typeof AccountSchema>
+
+export const AccountListRes = z.object({
+  data: z.array(AccountSchema),
+  message: z.string()
+})
+
+export type AccountListResType = z.TypeOf<typeof AccountListRes>
 
 export const AccountRes = z.object({
     data : AccountSchema,
@@ -15,3 +23,55 @@ export const AccountRes = z.object({
 }).strict()
 
 export type AccountResType = z.TypeOf<typeof AccountRes>
+
+export const CreateEmployeeAccountBody = z
+  .object({
+    name: z.string().trim().min(2).max(256),
+    email: z.string().email(),
+    avatar: z.string().url().optional(),
+    password: z.string().min(6).max(100),
+    confirmPassword: z.string().min(6).max(100)
+  })
+  .strict()
+  .superRefine(({ confirmPassword, password }, ctx) => {
+    if (confirmPassword !== password) {
+      ctx.addIssue({
+        code: 'custom',
+        message: 'Mật khẩu không khớp',
+        path: ['confirmPassword']
+      })
+    }
+  })
+
+export type CreateEmployeeAccountBodyType = z.TypeOf<typeof CreateEmployeeAccountBody>
+
+export const UpdateEmployeeAccountBody = z
+  .object({
+    name: z.string().trim().min(2).max(256),
+    email: z.string().email(),
+    avatar: z.string().url().optional(),
+    changePassword: z.boolean().optional(),
+    password: z.string().min(6).max(100).optional(),
+    confirmPassword: z.string().min(6).max(100).optional()
+  })
+  .strict()
+  .superRefine(({ confirmPassword, password, changePassword }, ctx) => {
+    if (changePassword) {
+      if (!password || !confirmPassword) {
+        ctx.addIssue({
+          code: 'custom',
+          message: 'Hãy nhập mật khẩu mới và xác nhận mật khẩu mới',
+          path: ['changePassword']
+        })
+      } else if (confirmPassword !== password) {
+        ctx.addIssue({
+          code: 'custom',
+          message: 'Mật khẩu không khớp',
+          path: ['confirmPassword']
+        })
+      }
+    }
+  })
+
+export type UpdateEmployeeAccountBodyType = z.TypeOf<typeof UpdateEmployeeAccountBody>
+
